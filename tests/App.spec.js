6e5cc28ad3f18b572a687a3b2211cd8d31e13f61
Tobechukwu Obitube <tobechukwuobitube@gmail.com>
2019-07-31 15:10:14 +0100

    chore(codeClimateCircleCI) Run codeclimate test coverage with circleCI
    - ensure that circleCI was integrated into the project
    - ensure that codeclimate was integrated into the project
    - ensure that ReadMe.md was updated with coverage badges
    
    Finishes [#166840877]

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -8,22 +8,22 @@ import HomePage from '../src/pages/hompage';
 import Routes from '../src/routes/AppRouter';
 
 describe('Application test', () => {
-  it('should work fine', () => {
+  it('should work fine on App', () => {
     const wrapper = shallow(<App />);
 
     expect(toJson(wrapper)).toMatchSnapshot();
   });
-  it('should work fine', () => {
+  it('should work fine on Login Page', () => {
     const wrapper = shallow(<LoginPage />);
 
     expect(toJson(wrapper)).toMatchSnapshot();
   });
-  it('should work fine', () => {
+  it('should work fine Home Page', () => {
     const wrapper = shallow(<HomePage />);
 
     expect(toJson(wrapper)).toMatchSnapshot();
   });
-  it('should work fine', () => {
+  it('should work fine on Routes', () => {
     const wrapper = shallow(<Routes />);
 
     expect(toJson(wrapper)).toMatchSnapshot();
