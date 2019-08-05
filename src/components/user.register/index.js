55fc41017c14f8f1ed3c5b5248a9e3ee6972a1e3
olorunwalawrence <olorunwalawrence5@gmail.com>
2019-08-05 17:28:16 +0100

    ft(register):user registration - create registration-template [Delivers #166840878]

---
+++ src/components/user.register/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+export default function UserRegidtration() {
+  return (
+    <div className="flex justify-center items-center bg-gray-200 h-screen">
+      <div className="md:flex max-w-lg w-7/12 bg-color height items-center auth-bg hidden md:block rounded-l-lg ">
+        <p className="px-5 text-center text-white text-3xl">
+          Welcome, Signup to share, recomend and bookmark
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LandingPage from '../pages/loginPage';
 import Homepage from '../pages/hompage';
+import User from '../components/user.register/index';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={Homepage} />
     <Route path="/login" exact component={LandingPage} />
+    <Route path="/user" exact component={User} />
   </Switch>
 );
 
