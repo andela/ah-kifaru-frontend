dc6d1392f03077399c7fc0d6faef86d9563abbcd
tejumola <tejumoladavid@gmail.com>
2019-08-23 15:15:53 +0100

    ft(hotfix):hotfix-for-public-path-webpack

--- src/components/userMenu/index.js
+++ src/components/userMenu/index.js
@@ -20,9 +20,17 @@ const Menu = () => (
       </Link>
     </li>
     <li className="p-3 whitespace-no-wrap text-sm md:text-base text-black  hover:text-gray-800 hover:bg-gray-100">
-      <Link className="px-2 py-1" to="/">
+      <button
+        type="button"
+        className="px-2 py-1"
+        onClick={() => {
+          localStorage.clear();
+          localStorage.removeItem('token');
+          window.location.replace('/');
+        }}
+      >
         <span>Logout</span>
-      </Link>
+      </button>
     </li>
   </ul>
 );
